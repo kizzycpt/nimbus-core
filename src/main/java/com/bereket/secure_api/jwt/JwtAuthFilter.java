b0cecd64a4831fe1a08fde73bc76085182db3cf7
sko <kizzycpt@proton.me>
2026-01-17 12:37:17 -0500

    Add shouldNotFilter method to JwtAuthFilter

--- src/main/java/com/bereket/secure_api/jwt/JwtAuthFilter.java
+++ src/main/java/com/bereket/secure_api/jwt/JwtAuthFilter.java
@@ -22,6 +22,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/api/auth/")
+            || path.equals("/health");
+    }
+    
+    @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
